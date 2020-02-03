2e48cfc2a7f8f151642c9f7b9b015b8da601e6ec
sunilnagargojecc <59916458+sunilnagargojecc@users.noreply.github.com>
2020-02-03 15:30:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -48,11 +48,23 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save);
+app.post('/journeybuilder/save/', (req, res) => {
+ res.send(req.body.filename + req.body.sourcename + req.body.sendername + req.body.msg);
+ 
+	
+ console.log('POST FORM BODY', req.body);
+});
+
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 
-app.post('/journeybuilder/execute/', activity.execute);
+app.post('/journeybuilder/execute/', (req, res) => {
+ res.send(req.body.filename + req.body.sourcename + req.body.sendername + req.body.msg);
+ 
+	
+ console.log('POST FORM BODY', req.body);
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
